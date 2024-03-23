7a3ab95ed2651cad901c85f279331e75affe40ad
Swarnim <swarnimchandve23@gmail.com>
2024-03-24 00:57:23 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { DarkModeProvider } from "./contexts/DarkMode";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    </BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
