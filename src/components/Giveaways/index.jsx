82659904036eb82daf64809d791e68f08b742bbd
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2024-03-26 20:06:15 +0530

    Added: components

---
+++ src/components/Giveaways/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Giveaways() {
+  return (
+    <div className="hero w-full h-full flex flex-col items-center justify-center">Giveways</div>
+  )
+}
--- src/pages/NewPage.jsx
+++ src/pages/NewPage.jsx
@@ -1,63 +1,96 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
+import Hero from "../components/Hero";
+import Speakers from "../components/Speakers";
+import Timer from "../components/Timer";
+import Giveaways from "../components/Giveaways";
+import Sponsors from "../components/Sponsors";
+import CommunityPartners from "../components/CommunityPartners";
+import SocialMedia from "../components/SocialMedia";
 
 export default function NewPage() {
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
-  ];
+  const layouts = {
+    lg: [
+      { i: "a", x: 0, y: 0, w: 1, h: 1 },
+      { i: "b", x: 1, y: 0, w: 1, h: 1 },
+      { i: "c", x: 2, y: 0, w: 2, h: 3 },
+      { i: "d", x: 4, y: 0, w: 2, h: 2 },
+      { i: "f", x: 0, y: 2, w: 2, h: 2 },
+      { i: "g", x: 4, y: 3, w: 2, h: 2 },
+      { i: "h", x: 0, y: 4, w: 2, h: 2 },
+      { i: "i", x: 2, y: 4, w: 2, h: 2 },
+      { i: "j", x: 4, y: 5, w: 2, h: 1 },
+    ],
+    md: [
+      { i: "a", x: 0, y: 0, w: 1, h: 2 },
+      { i: "b", x: 1, y: 1, w: 1, h: 3 },
+      { i: "c", x: 0, y: 0, w: 2, h: 6 },
+      { i: "d", x: 0, y: 4, w: 2, h: 4 },
+      { i: "f", x: 0, y: 5, w: 2, h: 6 },
+      { i: "g", x: 0, y: 6, w: 2, h: 7 },
+      { i: "h", x: 0, y: 7, w: 2, h: 5 },
+      { i: "i", x: 0, y: 8, w: 2, h: 5 },
+      { i: "j", x: 0, y: 9, w: 2, h: 3 },
+    ],
+  };
 
   const layoutRef = useRef(null);
-  const [height, setHeight] = useState(30); 
+  const [height, setHeight] = useState(30);
 
   useEffect(() => {
     // height of layoutRef
     setHeight(layoutRef.current.clientHeight / 6);
-  } ,[]);
+  }, []);
 
   const ResponsiveGridLayout = WidthProvider(Responsive);
 
-  
-
   return (
-    <div className="w-screen lg:h-screen px-4 lg:px-10 py-8" ref={layoutRef}>
-        <div className="w-20 aspect-square rounded-full bg-red-600 absolute top-[10%] left-[10%]">
-
-        </div>
+    <div className="lg:h-screen px-4 lg:px-4" ref={layoutRef}>
       <ResponsiveGridLayout
-        className="layout"
+        className="layout my-auto"
+        layouts={layouts}
         rowHeight={height}
         margin={[15, 15]}
         breakpoints={{ lg: 1024, md: 768, sm: 640, xs: 480, xxs: 0 }}
         cols={{ lg: 6, md: 1, sm: 1, xs: 1, xxs: 1 }}
+        compactType="vertical"
+        draggableHandle=".dragHandle"
       >
-        <div key="a" className="border-2 border-black rounded-xl transition-all duration-100 bg-[#e3e3e3] bg-opacity-20 backdrop-blur-md" data-grid={{ x: 0, y: 0, w: 1, h: 1, static: true }}>
-          Timer
+        <div key="a" className="container">
+          <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <Timer />
         </div>
-        <div key="b" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 1, y: 0, w: 1, h: 1 }}>
+        <div key="b" className="container">
+          <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
           Schedule
         </div>
-        <div key="c" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 2, y: 0, w: 2, h: 3 }}>
-          TechOtsav
+        <div key="c" className="container">
+          <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <Hero />
         </div>
-        <div key="d" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 4, y: 0, w: 2, h: 2 }}>
+        <div key="d" className="container">
+        <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
           Venue
         </div>
-        <div key="f" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 0, y: 2, w: 2, h: 2 }}>
-          Giveaways
+        <div key="f" className="container">
+        <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <Giveaways />
         </div>
-        <div key="g" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 4, y: 3, w: 2, h: 2 }}>
-          Speakers
+        <div key="g" className="container">
+        <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <Speakers />
         </div>
-        <div key="h" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 0, y: 4, w: 2, h: 2 }}>
-          Sponsors
+        <div key="h" className="container">
+        <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <Sponsors />
         </div>
-        <div key="i" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 2, y: 4, w: 2, h: 2 }}>
-          Community Partners
+        <div key="i" className="container">
+        <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <CommunityPartners />
         </div>
-        <div key="j" className="border-2 border-black rounded-xl transition-all duration-100" data-grid={{ x: 4, y: 5, w: 2, h: 1 }}>
-          Social Media
+        <div key="j" className="container">
+        <div className="dragHandle absolute left-0 right-0 w-12 h-1 mx-auto bg-blue-400 cursor-[grab] rounded-full"></div>
+          <SocialMedia />
         </div>
       </ResponsiveGridLayout>
     </div>
