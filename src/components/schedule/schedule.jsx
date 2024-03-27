5dc81d621f80356fc94fbdb273facbca9b93fed5
piyushcodes07 <piyushcodes07@gmail.com>
2024-03-27 23:59:51 +0530

    new schedule ready!

--- src/components/schedule/schedule.jsx
+++ src/components/schedule/schedule.jsx
@@ -1,24 +1,25 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import Grid from "@mui/material/Grid";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-
+// import Grid from "@mui/material/Grid";
+// import { styled } from "@mui/material/styles";
+// import Paper from "@mui/material/Paper";
+import closeButton from '../../assets/icon.png'
 const style = {
   position: "absolute",
-//   opacity: "0.7",
-backgroundColor:'#141414',
+  //   opacity: "0.7",
+  backgroundColor: "#141414",
   top: "50%",
   left: "50%",
+  
   transform: "translate(-50%, -50%)",
   width: "80%",
   height: "85%",
   borderLeft: "5px solid red",
   borderRadius: "2%",
-
+  overflowY:'auto ',
   border: "1px solid #000",
   boxShadow: 24,
   p: 4,
@@ -40,13 +41,13 @@ const largeStyle = {
   p: 4,
 };
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: "center",
+//   color: theme.palette.text.secondary,
+// }));
 
 export default function Schedule() {
   const [open, setOpen] = React.useState(false);
@@ -55,7 +56,7 @@ export default function Schedule() {
 
   return (
     <div>
-     <button onClick={handleOpen}  >click me modal ready!</button>
+      <button onClick={handleOpen}>click me modal ready!</button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -63,204 +64,145 @@ export default function Schedule() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style} lg={largeStyle}>
-          <dir>
-            <h1 style={{fontSize:'30px',textAlign:'center',padding:'5px'}}>Schedule</h1>
-            <Grid container spacing={2}>
-              <Grid style={{ opacity: "1" }} item xs={2}>
-                <Item
-                  style={{
-                    color: "white ",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    fontWeight: "normal",
-                    height: "80px",
+          <dir >
+            <div className=" w-[2%] h-[2%] fixed hover:cursor-pointer ">
+            <img  src={closeButton} onClick={handleClose} alt="close button" />
 
-                    
-                    textAlign: "center",
-                  }}
-                >
-                  9:00am - 10:00am
-                </Item>
-              </Grid>
-              <Grid style={{ height: "80px" }} item xs={5}>
-                <Item style={{
-                    color: "white ",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    fontWeight: "normal",
-                    height: "80px",
-
-                    borderLeft: "5px solid rgb(219, 68, 55)",
-                    textAlign: "center",
-                  }}>xs=4</Item>
-              </Grid>
-              <Grid item style={{ textAlign: "center" }} xs={5}>
-                <Item
-                  style={{
-                    borderLeft: "5px solid rgb(15, 157, 86)",
-                    backgroundColor: "rgb(29, 58, 46)",
-                    color: "white ",
-                        
-                    fontWeight: "normal",
-                    height: "80px",
-                    textAlign: "start",
-                  }}
-                >
-                  Breakfast ☕
-                </Item>
-              </Grid>
-              <Grid style={{ opacity: "1" }} item xs={2}>
-                <Item
-                  style={{
-                    color: "white ",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    fontWeight: "normal",
-                    height: "80px",
-
-                    
-                    textAlign: "center",
-                  }}
-                >
-                  9:00am - 10:00am
-                </Item>
-              </Grid>
-              <Grid item style={{ textAlign: "center" }} xs={10}>
-                <Item
-                  style={{
-                    borderLeft: "5px solid rgb(15, 157, 86)",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    color: "white ",
-                        
-                    fontWeight: "normal",
-                    height: "80px",
-                    textAlign: "start",
-                  }}
-                >
-               Free popcorn and movie
-                </Item>
-              </Grid>
-
-
-
-              <Grid style={{ opacity: "1" }} item xs={2}>
-                <Item
-                  style={{
-                    color: "white ",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    fontWeight: "normal",
-                    height: "80px",
-
-                    
-                    textAlign: "center",
-                  }}
-                >
-                  9:00am - 10:00am
-                </Item>
-              </Grid>
-              <Grid item style={{ textAlign: "center" }} xs={10}>
-                <Item
-                  style={{
-                    borderLeft: "5px solid rgb(66, 133, 244)",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    color: "white ",
-                        
-                    fontWeight: "normal",
-                    height: "80px",
-                    textAlign: "start",
-                  }}
-                >
-                 Timepass video games
-                </Item>
-              </Grid>
-
-              <Grid style={{ opacity: "1" }} item xs={2}>
-                <Item
-                  style={{
-                    color: "white ",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    fontWeight: "normal",
-                    height: "80px",
-
-                    
-                    textAlign: "center",
-                  }}
-                >
-                  9:00am - 10:00am
-                </Item>
-              </Grid>
-              <Grid item style={{ textAlign: "center" }} xs={10}>
-                <Item
-                  style={{
-                    borderLeft: "5px solid rgb(244, 180, 0)",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    color: "white ",
-                        
-                    fontWeight: "normal",
-                    height: "80px",
-                    textAlign: "start",
-                  }}
-                >
-                    <div style={{display:'flex',flexDirection:'row',gap:'7px'}}>
-                        <div style={{borderRadius:'50%',height:'50px',width:'50px',backgroundImage:`url(https://m.economictimes.com/thumb/msid-97021979,width-1200,height-900,resizemode-4,imgsize-46928/modi-new-pti1.jpg)`,backgroundRepeat: 'no-repeat',backgroundSize: 'cover' }}>
-
-                        </div>
-                        <div>
-                            <div>
-                            Narendra ModiJi
-                            </div>
-                            <div>
-                                chief minister of Lokmanya nagar
-                            </div>
-                        </div>
-                    </div>
-                 
-                </Item>
-              </Grid>
-              <Grid style={{ opacity: "1" }} item xs={2}>
-                <Item
-                  style={{
-                    color: "white ",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    fontWeight: "normal",
-                    height: "80px",
-
-                    
-                    textAlign: "center",
-                  }}
-                >
-                  9:00am - 10:00am
-                </Item>
-              </Grid>
-              <Grid item style={{ textAlign: "center" }} xs={10}>
-                <Item
-                  style={{
-                    borderLeft: "5px solid rgb(244, 180, 0)",
-                    backgroundColor: "rgb(40, 41, 43)",
-                    color: "white ",
-                        
-                    fontWeight: "normal",
-                    height: "80px",
-                    textAlign: "start",
-                  }}
-                >
-                    <div style={{display:'flex',flexDirection:'row',gap:'7px'}}>
-                        <div style={{borderRadius:'50%',height:'50px',width:'50px',backgroundImage:`url(https://m.media-amazon.com/images/M/MV5BMmM3MWU1NWQtMDc5ZC00YzgzLTljZGItOTVkMDJlNzkxY2MxXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_FMjpg_UX1000_.jpg)`,backgroundRepeat: 'no-repeat',backgroundSize: 'cover' }}>
-
-                        </div>
-                        <div>
-                            <div>
-                            Rahul gandhi
-                            </div>
-                            <div>
-                                Meme ministry of india
-                            </div>
-                        </div>
-                    </div>
-                 
-                </Item>
-              </Grid>
+            </div>
+            <h1
+              style={{ fontSize: "30px", textAlign: "center", padding: "5px" }}
+            >
+              Schedule
+            </h1>
             
+            <div className="w-12/12 md:w-7/12 lg:6/12 mx-auto relative ">
+              
+              <div className="border-l-2 mt-10">
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#4285F4] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#4285F4] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
 
+                  <div className="w-10 h-1 bg-blue-300 absolute -left-10 z-0"></div>
 
-            </Grid>
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#EA4335] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#EA4335] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-pink-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#FBBC05] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#FBBC05] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-green-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#2ECC71] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#2ECC71] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-purple-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#4285F4] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#4285F4] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-blue-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#EA4335] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#EA4335] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-pink-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#FBBC05] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#FBBC05] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-green-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-[#2ECC71] text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                  <div className="w-5 h-5 bg-[#2ECC71] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
+
+                  <div className="w-10 h-1 bg-purple-300 absolute -left-10 z-0"></div>
+
+                  <div className="flex-auto">
+                    <h1 className="text-lg">Day 1</h1>
+                    <h1 className="text-xl font-bold">
+                      Orientation and Briefing on Uniliver basics
+                    </h1>
+                    <h3>classNameroom</h3>
+                  </div>
+                  <div></div>
+                </div>
+                
+                
+              </div>
+              
+            
+            </div>
           </dir>
         </Box>
       </Modal>
