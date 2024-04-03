fb790ef08924208ae4fc34074150c1b182da663a
Swarnim <swarnimchandve23@gmail.com>
2024-04-03 10:34:27 +0530

    card2

--- src/components/Speakers/Card.jsx
+++ src/components/Speakers/Card.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import './Card.css';
+import React, { useState } from "react";
+import "./Card.css";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
-
 
 function Card(props) {
   const [showSocial, setShowSocial] = useState(false);
@@ -13,43 +12,61 @@ function Card(props) {
   };
 
   return (
-    <div className="container ">
-      <div className="card flex flex-wrap  w-max ">
-        <div className="card__border rounded-full p-1 mt-5 mr-10 hover:scale-105">
-          <img
-            src="https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg"
-            alt="card image"
-            className="card__img"
-          />
-        </div>
-        <div className='mt-12 '>
-          <h3 className="card__name text-xl">{props.name}</h3>
-          <span className="card__profession text-sm">{props.role}</span>
+    <div className="card flex flex-wrap  w-full  ">
+      <div className="card__border rounded-full p-1 mt-5 mr-10 hover:scale-105">
+        <img
+          src="https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg"
+          alt="card image"
+          className="card__img"
+        />
+      </div>
+      <div className="mt-12 ">
+        <h3 className="card__name text-xl">{props.name}</h3>
+        <span className="card__profession text-sm">{props.role}</span>
+      </div>
 
-        </div>
-
-        <div className={`card__social ${showSocial ? 'animation' : ''}`} id="card-social">
-          <div className="card__social-control">
-            <div className="card__social-toggle" onMouseEnter={toggleSocial} id="card-toggle">
-              <span className="ri--add-line"></span>
-            </div>
-
-            <span className="card__social-text text-sm font-semibold ">Social Networks</span>
-
-            <ul className="card__social-list">
-              <a href={props.Linkedin} target="_blank" className="card__social-link">
-              <FaLinkedin  />
-              </a>
-
-              <a href={props.Instagram} target="_blank" className="card__social-link">
-              <FaSquareInstagram />
-              </a>
-
-              <a href={props.Twitter} target="_blank" className="card__social-link">
-              <FaSquareXTwitter />
-              </a>
-            </ul>
+      <div
+        className={`card__social ${showSocial ? "animation" : ""}`}
+        id="card-social"
+      >
+        <div className="card__social-control">
+          <div
+            className="card__social-toggle"
+            onMouseEnter={toggleSocial}
+            id="card-toggle"
+          >
+            <span className="ri--add-line"></span>
           </div>
+
+          <span className="card__social-text text-sm font-semibold ">
+            Social Networks
+          </span>
+
+          <ul className="card__social-list">
+            <a
+              href={props.Linkedin}
+              target="_blank"
+              className="card__social-link"
+            >
+              <FaLinkedin />
+            </a>
+
+            <a
+              href={props.Instagram}
+              target="_blank"
+              className="card__social-link"
+            >
+              <FaSquareInstagram />
+            </a>
+
+            <a
+              href={props.Twitter}
+              target="_blank"
+              className="card__social-link"
+            >
+              <FaSquareXTwitter />
+            </a>
+          </ul>
         </div>
       </div>
     </div>
--- src/components/Speakers/index.jsx
+++ src/components/Speakers/index.jsx
@@ -6,14 +6,14 @@ export default function Speakers() {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold bg-gradient-to-r from-[var(--google-red)] to-[var(--google-blue)] inline-block text-transparent bg-clip-text">
+      <h1 className="text-3xl font-bold bg-gradient-to-r from-[var(--google-red)] to-[var(--google-blue)] inline-block text-transparent bg-clip-text">
         Speakers
       </h1>
       <br />
       {/* <p className="">
         Revealing Soon...
       </p>  */}
-      <Card/>
+      <Card name="Aman Tiwari" role="Developer"/>
     </div>
   );
 }
